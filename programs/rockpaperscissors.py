5a713fcb897b5e80ca6f23b7342dfda400eeb959
Manmeet Singh <120583077+thatsmanmeet@users.noreply.github.com>
2023-01-20 13:05:49 +0530

    Improved Rock Paper Scissors Game.

--- programs/rockpaperscissors.py
+++ programs/rockpaperscissors.py
@@ -1,5 +1,13 @@
 import random
 
+# Terminal Colors
+CRED = '\033[91m'
+CYELLOW = '\33[33m'
+CGREEN  = '\33[32m'
+CGREENBG  = '\33[42m'
+CBLUEBG   = '\33[44m'
+CEND = '\033[0m'
+
 def choosed_item_name(item_name):
     match item_name: 
         case "R" : return "Rock"
@@ -12,54 +20,56 @@ def run_game():
     choices = ["R","P","S"]
     round = 1
     while round <= 5:
-        print(f"Round {round}")
-        your_choice = input("Enter R for rock, S for scissors, P for paper: ").upper()
+        print(CBLUEBG +f"\n\t\t*** Round {round} ***" + CEND)
+        your_choice = input("\nEnter R for rock, S for scissors, P for paper: ").upper()
         computers_choice = random.choice(choices)
         print(f"You Choose {choosed_item_name(your_choice)} - Computer choose {choosed_item_name(computers_choice)}")
         # draw conditions
-        if your_choice == "R" and computers_choice == "R":
-            print(f"Draw - Score {computers_score} - {my_score}")
-        elif your_choice == "S" and computers_choice == "S":
-            print(f"Draw - Score {computers_score} - {my_score}")
-        elif your_choice == "P" and computers_choice == "P":
-            print(f"Draw - Score {computers_score} - {my_score}")   
+        if your_choice == computers_choice:
+            print(CYELLOW+f"Draw - Score {computers_score} - {my_score}"+CEND) 
         # Computer's win situation 
         elif your_choice == "S" and computers_choice == "R":
             computers_score += 1
-            print(f"Computer Win - Score {computers_score} - {my_score}")
+            print(CRED+f"Computer Win - Score {computers_score} - {my_score}"+CEND)
         elif your_choice == "R" and computers_choice == "P":
             computers_score += 1
-            print(f"Computer Win - Score {computers_score} - {my_score}")
+            print(CRED+f"Computer Win - Score {computers_score} - {my_score}"+CEND)
         elif your_choice == "P" and computers_choice == "S":
             computers_score += 1
-            print(f"Computer Win - Score {computers_score} - {my_score}")
+            print(CRED+f"Computer Win - Score {computers_score} - {my_score}"+CEND)
         # You Win
         elif your_choice == "S" and computers_choice == "P":
             my_score += 1
-            print(f"You Win - Score {computers_score} - {my_score}")
+            print(CGREEN+f"You Win - Score {computers_score} - {my_score}"+CEND)
         elif your_choice == "P" and computers_choice == "R":
             my_score += 1
-            print(f"You Win - Score {computers_score} - {my_score}")
+            print(CGREEN+f"You Win - Score {computers_score} - {my_score}"+CEND)
         elif your_choice == "R" and computers_choice == "S":
             my_score += 1
-            print(f"You Win - Score {computers_score} - {my_score}")     
+            print(CGREEN+f"You Win - Score {computers_score} - {my_score}"+CEND)    
         round+=1
 
     # Print's final game score at the end....
     if my_score > computers_score:
-            print(f"\nYOU WIN - [{my_score} : {computers_score}]")
+            print(CGREEN +f"\n\t\t|| YOU WIN [{my_score} : {computers_score}] ||" + CEND)
     elif computers_score > my_score:
-            print(f"\nCOMPUTER WIN - [{my_score} : {computers_score}]")
+            print(CRED + f"\n\t\t|| COMPUTER WIN [{my_score} : {computers_score}] ||" + CEND)
     else:
-            print(f"\nDRAW - [{my_score} : {computers_score}]")
+            print(CYELLOW +f"\n\t\t|| DRAW [{my_score} : {computers_score}] ||" + CEND)
         
-    print("---- Game Over -----")
+    print(CRED + "\n\t\t Game Over " + CEND)
 
 
-print("-------ROCK PAPER SCISSORS GAME-------")
+print(CGREENBG + "\n\t   ROCK PAPER SCISSORS GAME   " + CEND)
+print(CBLUEBG + "\n  This is a 5 round Rock Paper Scissors Game built in python.   "+ CEND)
 while True:
     run_game()
-    choice = input("Do you want to play again (y/n): ")
+    choice = input("\nDo you want to play again (y/n): ")
     if choice == "n":
-        print("Exiting the game...")
+        print(CRED + "Exiting the game..." + CEND)
+        break
+    elif choice == "y":
+        continue
+    else:
+        print(CRED + "You entered invalid operation. Restart the game." + CEND)
         break
