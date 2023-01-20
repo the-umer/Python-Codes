5a2e18c9fb240eafc8d627548ea9b55ae2861756
Manmeet Singh <120583077+thatsmanmeet@users.noreply.github.com>
2023-01-20 15:58:35 +0530

    update readme and python program.

--- articlescraper.py
+++ articlescraper.py
@@ -13,7 +13,8 @@ from bs4 import BeautifulSoup
 # Terminal Colors
 CRED = '\033[91m' # Red color is for the headings!
 CYELLOW = '\33[33m' # Yellow color is for the paragraphs!
-CGREENBG  = '\33[42m' # Green Background Color!
+CYELLOWBG  = '\33[43m' # Green Background Color!
+CREDBG2    = '\33[41m' # Red Background Color!
 CEND = '\033[0m'
 
 
@@ -21,10 +22,11 @@ def getHtmlFile(url):
     req = requests.get(url)
     return req.text # this will return entire HTML document
 
-
 def getArticleData():
-    print("\n"+CGREENBG + "Python Article Scraper" + CEND+"\n")
-    input_url = input("Paste a url: ")
+    print("\n"+CYELLOWBG + "📝 Python Article Scraper 📝" + CEND+"\n")
+    print("\n"+ CREDBG2 +"This program may not work on all the websites as some websites might ban the use of these modules to scrap their content and also scraping might be illegal and this program is only built for educational purposes and you hold the responsibility of using it. Use it carefully!" + CEND + "\n")
+    input_url = input("Paste a url 🌎: ")
+    print(CYELLOW + "Please wait, I'm trying to fetch the article. This can take a minute or two depening on your's/server's internet speed." + CEND + "\n")
     soup = BeautifulSoup(getHtmlFile(input_url),'html.parser')
     for headers in soup.find_all(['h1','h2','h3','h4']):
         print(CRED +headers.get_text()+ CEND + '\n')
@@ -42,6 +44,7 @@ while True:
     if choice == "y":
         continue
     elif choice == "n":
+        print(CRED + "Exiting the Program, See you soon!" + CEND)
         break
     else:
         print(CRED + "Invalid Selector, Exiting the program..." + CEND)
